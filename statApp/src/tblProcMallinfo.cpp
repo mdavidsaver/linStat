dc73db833149a8b4c2593b082b30b9a3ef90edc1
Michael Davidsaver <mdavidsaver@gmail.com>
2024-06-21 10:23:02 -0700

    mallinfo2()

---
+++ statApp/src/tblProcMallinfo.cpp
@@ -0,0 +1,52 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * linStat is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+/* call mallinfo2()
+ */
+
+#include <malloc.h>
+
+#include <dbDefs.h>
+#include <errlog.h>
+
+#include "linStat.h"
+
+namespace {
+using namespace linStat;
+
+const char * const tblName = "mallinfo";
+
+struct MallInfoTable : public StatTable {
+
+    explicit MallInfoTable(const std::string& inst, const Reactor& react)
+        :StatTable(tblName, inst, react)
+    {
+        if(inst!="self")
+            throw std::runtime_error("Only 'self' supported");
+    }
+    virtual ~MallInfoTable() {}
+
+    virtual void update() override final {
+        Transaction tr(*this);
+
+        auto info(mallinfo2());
+#define CASE(NAME) tr.set(#NAME, info.NAME)
+        CASE(arena);
+        CASE(ordblks);
+        //CASE(smblks); // unused
+        CASE(hblks);
+        CASE(hblkhd);
+        //CASE(usmblks); // unused
+        //CASE(fsmblks); // unused
+        CASE(uordblks);
+        CASE(fordblks);
+        CASE(keepcost);
+#undef CASE
+    }
+};
+
+} // namespace
+
+DEFINE_TABLE(tblName, MallInfoTable)
